bebec7690989f6cc5f81bf6ea437900f3185856e
so-a-ud <bezzari.souad@gmail.com>
2021-01-05 12:47:39 +0100

    creation table Experimental

---
+++ CV_THEQUE/database/migrations/2021_01_05_114316_create_experimentals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExperimentalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experimentals', function (Blueprint $table) {
+            $table->id();
+            $table->Date('DateDebut');
+            $table->Date('DateFin');
+            $table->String('Societe',500);
+            $table->integer('cv_id')->unsigned();
+            $table->foreign('cv_id')->references('id')->on('curriculum_vitae')->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experimentals');
+    }
+}
